869df7431ac8385e175ba76ab7df4b7769b4e0e7
Nitu Balwani <balwaninitu@gmail.com>
2021-11-18 14:26:09 +0800

    Scanner input output

---
+++ Scanner.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+class Scanners {
+
+	public static void main(String[] args) {
+		
+		int a;
+		System.out.print("Enter Data ");
+
+		Scanner obj=new Scanner(System.in);
+		a=obj.nextInt();
+		System.out.print("Get Data "+a);
+	}
+}
