c09db2d7d6938f2806a6fd92207122fa6bda1935
Nitu Balwani <balwaninitu@gmail.com>
2021-11-18 13:31:53 +0800

    Explicit Typecasting

--- Typecasting.java
+++ Typecasting.java
@@ -4,8 +4,8 @@ class TypeCasting{
 
 	 public static void main(String[] args) {
 
-	 	int a=10;  //4 Bytes size
-	 	double b=a;  //8 Bytes size int will convert to double(10.0)
+	 	int d=10;  //4 Bytes size
+	 	double b=d;  //8 Bytes size int will convert to double(10.0)done by compiler
 
 	 	System.out.print(b);
 
---
+++ Typecasting1.java
@@ -0,0 +1,15 @@
+
+//Explicit Typecasting
+
+class Typecasting1{
+
+	public static void main(String[] args) {
+		double a=10.5;  //8 Bytes
+		int b=(int)a;   //4 Bytes
+
+		System.out.print(b);
+
+	}
+
+
+}
