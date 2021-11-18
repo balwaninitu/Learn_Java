3e6e203de57945fd1396d808f4111fd242933119
Nitu Balwani <balwaninitu@gmail.com>
2021-11-18 13:20:22 +0800

    Implicit Typecasting

---
+++ Typecasting.java
@@ -0,0 +1,15 @@
+//Implicit Typecasting
+
+class TypeCasting{
+
+	 public static void main(String[] args) {
+
+	 	int a=10;  //4 Bytes size
+	 	double b=a;  //8 Bytes size int will convert to double(10.0)
+
+	 	System.out.print(b);
+
+
+		
+	}
+}
