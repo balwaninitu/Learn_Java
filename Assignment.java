a7db28f6d7f8a0a58452558a1bfb17d4417eb409
Nitu Balwani <balwaninitu@gmail.com>
2021-11-19 09:14:40 +0800

    Assignment operator

---
+++ Assignment.java
@@ -0,0 +1,30 @@
+/* Assignment Operator Example..!
+
+It is also unary operator so can work on single
+operand. There are two types
+Type1 = Simple
+Type2 = compound
+
+
+*/
+
+class Assignment{
+
+	public static void main(String[] args) {
+		
+		int a;
+		a=10;  //simple 
+
+		System.out.println(a);
+
+		a+=10; //compound
+
+		System.out.println(a);
+
+		a-=10; //compound
+
+		System.out.println(a);
+
+	}
+	
+}
