a786de4337146001b8a7a3099e542c84e10fc5a7
Nitu Balwani <balwaninitu@gmail.com>
2021-11-20 13:52:59 +0800

    Ternary operator

---
+++ Ternary.java
@@ -0,0 +1,16 @@
+
+/*Ternary operator is used in some condition hence its called conditinal operator
+
+*/
+
+class Ternary{
+
+public static void main(String[] args) {
+	
+	int a=10, b=20, Max;
+	//condition
+	Max=(a>b) ? a : b;
+	System.out.print(Max);
+}
+
+}
