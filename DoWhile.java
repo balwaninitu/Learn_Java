941512e151b3d283e2a63bde0101d4a4e21a0473
Nitu Balwani <balwaninitu@gmail.com>
2021-11-21 16:17:45 +0800

    Do while loop

---
+++ DoWhile.java
@@ -0,0 +1,21 @@
+/* do while loop is a post test loop
+here it executes statement first then it test the condition
+It is used when we want to executes loop body atleast once.
+even condition is false.
+It is also known as exit control loop.
+
+
+*/
+
+class DoWhile{
+	
+	public static void main(String[] args) {
+		int n=1;
+		do
+		{
+			System.out.print(n);
+		}
+		while(n<0);
+	}
+}
+
