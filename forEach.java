cbb0d8b2f352788e4c718df0d0be5ed069a039f6
Nitu Balwani <balwaninitu@gmail.com>
2021-11-21 16:44:36 +0800

    For each loop

---
+++ forEach.java
@@ -0,0 +1,19 @@
+/*
+for each loop mainly used to fetch the 
+values from a collection like array.
+*/
+
+class forEach
+{
+	public static void main(String[] args) {
+		
+		int a[]={10,20,30,40,50};
+
+		for ( int b : a) {
+
+			System.out.print(b + " ");
+
+		}
+	}
+
+}
