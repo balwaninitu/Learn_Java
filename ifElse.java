21ed32b8fa256b0b9c079c351865d0fcdf8b01dc
Nitu Balwani <balwaninitu@gmail.com>
2021-11-21 10:37:13 +0800

    If else statement

---
+++ ifElse.java
@@ -0,0 +1,26 @@
+//if else statement
+
+import java.util.Scanner;
+
+class ifElse{
+
+public static void main(String[] args) {
+
+	int n;
+	System.out.println("Enter any Numbers..  ");
+	Scanner r=new Scanner(System.in);
+	n=r.nextInt();
+
+	if(n>=0)
+	{
+
+	System.out.println("+ve Number");	
+	}
+	else
+	{
+		System.out.println("-ve Number");	
+	}
+	
+}
+
+}
