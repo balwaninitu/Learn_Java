8f533d37f9b6733ce701ce6289b14c5157b61ca1
Nitu Balwani <balwaninitu@gmail.com>
2021-11-21 16:31:59 +0800

    For loop

---
+++ ForLoop.java
@@ -0,0 +1,19 @@
+/*It is used when we want to perform initialization, 
+condition and incr/dcr operation in single line.
+It is also called entry control loop
+
+*/
+
+
+class ForLoop{
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=10; i++){
+
+			System.out.print(i + " ");
+
+		}
+	}
+	
+}
